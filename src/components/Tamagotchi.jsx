4426a62c4fc26ce16dba30392d6c211b95026ede
Erik Irgens and Kelar Crisp <erik@>
2019-09-11 16:16:55 -0700

    Add randomized tamagotchi images to the application, allowing for the tamagotchi to display one of six images as their image.

--- src/components/Tamagotchi.jsx
+++ src/components/Tamagotchi.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import fox from '../images/fox.png';
+import glob from '../images/glob.png';
+import scarecrow from '../images/scarecrow.png';
+import giphy from '../images/giphy.gif';
+import franco from '../images/franco.gif';
+import something from '../images/something.gif';
 
 let Tamagotchi = (props) => {
+
+    const imageArray = [fox, glob, scarecrow, giphy, franco, something];
+    const tamagotchiStyle = {
+        width: "20vw",
+        height: "auto"
+    }
     return (
         <div>
             <h2>{props.name} - {props.formattedAge} old.</h2>
             <h5>A {props.gender} tamagotchi.</h5>
+            <img style={tamagotchiStyle} src={imageArray[Math.floor(Math.random() * (5 - 0))]} />
             <p>{props.description}</p>
         </div>
     )
